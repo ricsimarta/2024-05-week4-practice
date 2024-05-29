48d3fc682c97e01685c77ba632a6e9b827ce92ed
ricsimarta <ricsimarta@gmail.com>
2024-05-29 18:07:44 +0200

    added hello world functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+console.log("hello world");
+
+helloWorld();
+
+function helloWorld() {
+  console.log("hello world from the function");
+  
+  helperFunction();
+
+  function helperFunction() {
+    console.log("i am helping");
+  }
+  
+  const functionVariable = "text"
+  console.log(functionVariable);
+}
+
+console.log("hello world 2");
+
+const coolerHelloWorld = function() {
+  console.log("hello world from the cooler function");
+}
+
+coolerHelloWorld();
